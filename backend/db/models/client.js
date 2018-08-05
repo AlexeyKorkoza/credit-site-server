c077085cdeb7b40849be53e72776d11172588ade
AlexeyKorkoza <alexeykorkoza@gmail.com>
2018-08-05 16:08:05 +0300

    Add model & migration for client

---
+++ backend/db/models/client.js
@@ -0,0 +1,55 @@
+module.exports = (sequelize, DataTypes) => {
+    const Client = sequelize.define('Client', {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        passport_data: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+        },
+        phone: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+        },
+        is_blocked: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+        },
+        territory: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.literal('CURRENT_TIMESTAMP'),
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.literal('CURRENT_TIMESTAMP'),
+        },
+    });
+
+    Client.associate = models => {
+        Client.belongsTo(models.Admin, {
+            foreignKey: 'admin_id',
+        });
+
+        Client.belongsTo(models.Manager, {
+            foreignKey: 'manager_id',
+        });
+    };
+
+    return Client;
+};
