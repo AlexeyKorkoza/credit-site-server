e1c473bf014fb206aee80f075bfa38788c0693e3
AlexeyKorkoza <alexeykorkoza@gmail.com>
2018-10-10 23:39:16 +0300

    #7 - Change passwords for all Managers

--- backend/controllers/api/managers.js
+++ backend/controllers/api/managers.js
@@ -1,4 +1,5 @@
 import { getModel } from '../../services/models';
+import { checkOldPasswords } from '../../services/passwords';
 
 const updateProfileManager = (req, res) => {
     const {
@@ -49,6 +50,63 @@ const updateProfileManager = (req, res) => {
         }));
 };
 
+const changePassword = (req, res) => {
+    const { oldPassword, newPassword } = req.body;
+    const { id, role } = req.user;
+
+    // @TODO Add validation
+
+    const model = getModel(role);
+    const data = {
+        password: newPassword,
+    };
+    const query = {
+        where: {
+            id,
+        },
+        plain: true,
+    };
+
+    return model.findOne(query)
+        .then(result => {
+            if (!result) {
+                return res.status(400).json({
+                    ok: 0,
+                    message: `${role} is not found`,
+                });
+            }
+
+            const { password } = result;
+            const isCompare = checkOldPasswords(oldPassword, password);
+            if (!isCompare) {
+                return res.status(400).json({
+                    ok: 0,
+                    message: 'Old password is incorrect',
+                });
+            }
+
+            return model.update(data, query);
+        })
+        .then(result => {
+            if (result) {
+                return res.status(200).json({
+                    ok: 1,
+                    message: 'Password is updated',
+                });
+            }
+
+            return res.status(400).json({
+                ok: 0,
+                message: 'Password is not updated',
+            });
+        })
+        .catch(err => res.status(500).json({
+            ok: 0,
+            message: err.message,
+        }));
+};
+
 export {
     updateProfileManager,
+    changePassword,
 };
--- backend/routes/api/managers.js
+++ backend/routes/api/managers.js
@@ -2,10 +2,12 @@ import express from 'express';
 
 import {
     updateProfileManager,
+    changePassword,
 } from '../../controllers/api/managers';
 
 const route = express.Router();
 
 route.put('/managers/update-profile', updateProfileManager);
+route.put('/managers/change-password', changePassword);
 
 export default route;
