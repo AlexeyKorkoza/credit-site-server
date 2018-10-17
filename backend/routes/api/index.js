41dc6d339fc6cec0778ee8eecdc6f5ff3fbcd912
AlexeyKorkoza <alexeykorkoza@gmail.com>
2018-10-17 22:44:39 +0300

    #7 - Update territorial coefficient

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,11 +3,13 @@ import express from 'express';
 import users from './users';
 import managers from './managers';
 import clients from './clients';
+import clientCards from './client_cards';
 
 const route = express.Router();
 
 route.use('/', users);
 route.use('/', managers);
 route.use('/', clients);
+route.use('/', clientCards);
 
 export default route;
