ae5684b27a79803d2fc18c499190747712bb029b
AlexeyKorkoza <alexeykorkoza@gmail.com>
2018-10-27 21:46:58 +0300

    Move work with db in business, use other line separator

--- backend/src/business/api/managers.js
+++ backend/src/business/api/managers.js
@@ -19,6 +19,41 @@ const makeBlockingOfManager = (adminId, managerId) => {
     return Manager.update(data, query);
 };
 
+const increaseInputCount = (login, user) => {
+    const query = {
+        where: {
+            login,
+        },
+    };
+
+    const data = Object.assign({},
+        user,
+        { input_count: user.input_count + 1 },
+    );
+    if (data.input_count > 4) {
+        data.is_blocked = true;
+    }
+
+    return Manager.update(query, data);
+};
+
+const authManager = (user, login) => {
+    const query = {
+        where: {
+            login,
+        },
+    };
+
+    const data = Object.assign({},
+        user,
+        { input_count: 0 },
+    );
+
+    return Manager.update(query, data);
+};
+
 export {
     makeBlockingOfManager,
+    increaseInputCount,
+    authManager,
 };
