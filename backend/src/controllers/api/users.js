3f515b9a54aaf6dd5200fa5220dada3b5eabb1de
AlexeyKorkoza <alexeykorkoza@gmail.com>
2018-10-27 21:46:58 +0300

    Move work with db in business, use other line separator

--- backend/src/controllers/api/users.js
+++ backend/src/controllers/api/users.js
@@ -1,1 +1,26 @@
-import { getModel } from '../../services/models';const createUser = (req, res) => {    const { role } = req.body;    const model = getModel(role);    let data;    if (role === 'manager') {        data = Object.assign({}, req.body, { full_name: req.body.fullName });    } else {        data = Object.assign({}, req.body);    }    return model.create(data)        .then(result => {            if (result) {                return res.status(201).json({                    ok: 1,                    message: `${role} is created`,                });            }            return res.status(400).json({                ok: 0,                message: `${role} is not created`,            });        })        .catch(err => res.status(500).json({            ok: 0,            message: err.message,        }));};export {    createUser,};
+import { makeCreatingUser } from '../../business/api/users';
+
+const createUser = (req, res) => {
+    return makeCreatingUser(req.body)
+        .then(result => {
+            if (result) {
+                return res.status(201).json({
+                    ok: 1,
+                    message: `${req.body.role} is created`,
+                });
+            }
+
+            return res.status(400).json({
+                ok: 0,
+                message: `${req.body.role} is not created`,
+            });
+        })
+        .catch(err => res.status(500).json({
+            ok: 0,
+            message: err.message,
+        }));
+};
+
+export {
+    createUser,
+};
