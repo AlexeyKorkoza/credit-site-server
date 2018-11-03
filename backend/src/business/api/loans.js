4f4048efafd69df89af3e75827b3174317c2d726
AlexeyKorkoza <alexeykorkoza@gmail.com>
2018-11-03 16:47:54 +0300

    #23 - Create loan

--- backend/src/business/api/loans.js
+++ backend/src/business/api/loans.js
@@ -1,5 +1,25 @@
 import { Loan } from '../../models/index';
 
+const makeCreating = body => {
+    const {
+        amount,
+        coefficient,
+        dateIssue,
+        dateMaturity,
+        totalRepaymentAmount,
+    } = body;
+
+    const data = {
+        amount,
+        coefficient,
+        data_issue: dateIssue,
+        date_maturity: dateMaturity,
+        total_repayment_amount: totalRepaymentAmount,
+    };
+
+    return Loan.create(data);
+};
+
 const makeUpdating = (id, body) => {
     const {
         amount,
@@ -45,6 +65,7 @@ const makeUpdatingIssueLoan = (id, body) => {
 };
 
 export {
+    makeCreating,
     makeUpdating,
     makeUpdatingIssueLoan,
 };
--- backend/src/routes/api/loans.js
+++ backend/src/routes/api/loans.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
 import {
+    createLoan,
     updateLoan,
     updateIssueLoan,
 } from '../../controllers/api/loans';
 
 const route = express.Route();
 
+route.post('/loans', createLoan);
 route.put('/loans/:id', updateLoan);
 route.put('/loans/:id/issue', updateIssueLoan);
 
