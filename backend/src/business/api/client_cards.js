e6b78d7aeba377e63478e64aea351f00867b49f5
AlexeyKorkoza <alexeykorkoza@gmail.com>
2018-11-03 17:01:27 +0300

    #23 - Modify customer data

--- backend/src/business/api/client_cards.js
+++ backend/src/business/api/client_cards.js
@@ -1,5 +1,33 @@
 import { ClientCard } from '../../models/index';
 
+const makeUpdatingClientCard = (id, body) => {
+    const {
+        fullName: full_name,
+        passportData: passport_data,
+        phone,
+        email,
+        surchargeFactor: surcharge_factor,
+        territory,
+    } = body;
+
+    const query = {
+        where: {
+            id,
+        },
+    };
+
+    const data = {
+        full_name,
+        passport_data,
+        phone,
+        email,
+        surcharge_factor,
+        territory,
+    };
+
+    return ClientCard.update(data, query);
+};
+
 const makeUpdatingTerritorialCoefficient = (id, surchargeFactor) => {
     const query = {
         where: {
@@ -14,5 +42,6 @@ const makeUpdatingTerritorialCoefficient = (id, surchargeFactor) => {
 };
 
 export {
+    makeUpdatingClientCard,
     makeUpdatingTerritorialCoefficient,
 };
--- backend/src/routes/api/client_cards.js
+++ backend/src/routes/api/client_cards.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import {
+    updateClientCard,
     updateTerritorialCoefficient,
 } from '../../controllers/api/client_cards';
 
 const route = express.Route();
 
+route.put('/client-cards/:id/', updateClientCard);
 route.put('/client-cards/:id/coefficient', updateTerritorialCoefficient);
 
 export default route;
