aeef336b00d2c661ff3bd570268a52a26bb7c95d
AlexeyKorkoza <alexeykorkoza@gmail.com>
2018-12-09 11:41:33 +0300

    #31 - Add middlewares to manager's possibilities

--- backend/src/routes/api/loans.js
+++ backend/src/routes/api/loans.js
@@ -5,11 +5,18 @@ import {
     updateLoan,
     updateIssueLoan,
 } from '../../controllers/api/loans';
+import { jwtMiddleware } from '../../middlewares/jwt';
+import authMiddleware from '../../middlewares/auth';
 
 const route = express.Route();
 
-route.post('/loans', createLoan);
+route.post('/loans',
+    jwtMiddleware,
+    authMiddleware.isManager,
+    createLoan);
+
 route.put('/loans/:id', updateLoan);
+
 route.put('/loans/:id/issue', updateIssueLoan);
 
 export default route;
