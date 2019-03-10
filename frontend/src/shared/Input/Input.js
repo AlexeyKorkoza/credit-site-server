dcda22cd08937548ad4d7f28db05163509469b88
AlexeyKorkoza <alexeykorkoza@gmail.com>
2019-03-10 11:00:22 +0300

    #40 - Delete unnecessary single quotes

--- frontend/src/shared/Input/Input.js
+++ frontend/src/shared/Input/Input.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Input = styled.input`
     outline: none;
     border-radius: 5px;
-    padding: '10px';
+    padding: 10px;
     width: 95%;
     border: none;
     `;
