5595c78cf798fee7976b52d56e295d0e8ee859c6
AlexeyKorkoza <alexeykorkoza@gmail.com>
2019-07-03 16:18:34 +0300

    #58 - Send role into component

--- frontend/src/containers/Loans/List.js
+++ frontend/src/containers/Loans/List.js
@@ -3,25 +3,30 @@ import { withRouter } from "react-router";
 
 import { getAllLoans } from '../../api/loans';
 import { List } from '../../components/Loans';
+import { getDataAuthUser } from "../../services/localDb";
 
 class LoansList extends Component{
     state = {
         loans: [],
+        role: null,
     };
 
     componentDidMount() {
         getAllLoans()
             .then(result => {
-                this.setState({ loans: result.loans });
+                const { role } = getDataAuthUser();
+
+                this.setState({ loans: result.loans, role });
             });
     }
 
     render() {
-        const { loans } = this.state;
+        const { loans, role } = this.state;
 
         return (
             <List
                 loans={loans}
+                role={role}
             />
         )
     }
